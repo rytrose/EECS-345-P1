7c70cb3241d2f8316a615ae1a6c2e3b282f1d1ce
Aaron Magid <ahm64@case.edu>
2017-02-19 23:01:30 -0500

    Added getVal.

--- p1.scm
+++ p1.scm
@@ -16,6 +16,38 @@
 ; inputs:
 ;  pt - parse tree
 ;  s - state
+; state structure:
+;  ( (list of variable names) (list of values) )
+;  values are empty list when not initialized
 (define interpret
   (lambda (pt s)
     display pt ))
+
+; getVal
+; Wrapper method for getVal* to deconstruct state variable as necessary
+; inputs:
+;  name - the name of the variable to find
+;  state - the state to look in
+; returns:
+;  --See return values for getVal*
+(define getVal
+  (lambda (name state)
+    (getVal* name (car state) (cadr state))))
+
+; getVal*
+; Gets the value of a given variable
+; inputs:
+;  name - the name of the variable to find
+;  vars - the list of variable names from the current state
+;  vals - the list of values in the current state
+; returns:
+;  Value of variable, if initialized
+;  '() if defined but not initialized
+;  #f if not defined
+(define getVal*
+  (lambda (name vars vals)
+    (cond
+      ((and (null? vars) (null? vals)) #f)
+      ((and (not (null? vars)) (not (null? vals)))
+       (if (eqv? name (car vars)) (car vals) (getVal* name (cdr vars) (cdr vals))))
+      (else (error "STATE MISMATCH ERROR: Different number of Variables and Values.")))))
