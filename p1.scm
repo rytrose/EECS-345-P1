d5d493efce9a66d5e28fa1c9f69a9075b64bec63
Ryan Rose <rytrose@gmail.com>
2017-02-19 23:09:39 -0500

    added untested setVal

--- p1.scm
+++ p1.scm
@@ -10,7 +10,7 @@
 ;   fd - file name of code to be interpreted
 (define interpreter
   (lambda (fd)
-    (interpret (parser fd) '(() ())) ))
+    (interpret (parser fd) '(() ()) ) ))
 
 ; interpret
 ; inputs:
@@ -18,4 +18,26 @@
 ;  s - state
 (define interpret
   (lambda (pt s)
-    display pt ))
+    ()))
+
+; setVal
+; inputs:
+;  name - variable name
+;  value - variable value
+;  state - the current state
+; outputs:
+;  the updated state
+(define setVal 
+  (lambda (name value state)
+    (cond
+      ; if name or value are null, error
+      ((or (null? name) (null? value)) (error "failed adding variable to state"))
+      ; if the var name already exists, error
+      ((not (eqv? (getVal name state) #f)) (error "namespace for var already occupied"))
+      ; add to state
+      ; 
+      (else
+       ; add name and value to state
+       (cons value (cdr (cons name (car state)))) ))))
+    
+
