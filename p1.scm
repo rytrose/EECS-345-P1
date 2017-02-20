42af56c93ab1d71d55b68b1c8d83512d599d1f92
Ryan Rose <rytrose@gmail.com>
2017-02-19 22:16:23 -0500

    adding Ben Aaron

--- p1.scm
+++ p1.scm
@@ -1,2 +1,21 @@
 ; Programming Project, Part 1
 ; Ryan Rose, rtr29
+
+; Load the parser and lexical analyzer
+(load "simpleParser.scm")
+(load "lex.scm")
+
+; interpreter:
+; inputs:
+;   fd - file name of code to be interpreted
+(define interpreter
+  (lambda (fd)
+    (interpret (parser fd) '(() ())) ))
+
+; interpret
+; inputs:
+;  pt - parse tree
+;  s - state
+(define interpret
+  (lambda (pt s)
+    display pt ))
